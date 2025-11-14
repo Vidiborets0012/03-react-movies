51beb2e7064250d61cc93215b6b6c9495109ada3
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-14 17:36:54 +0200

    implement and test the fetchMovies service

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "modern-normalize";
 
+import fetchMovies from "./services/movieService";
+
+fetchMovies("avatar").then((data) => {
+  console.log("MOVIES:", data);
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
