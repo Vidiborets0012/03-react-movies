62451f67a0bc12b8b3424fe3052a422da5611e1a
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-14 17:50:02 +0200

    create a Loader component

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "modern-normalize";
 
-import fetchMovies from "./services/movieService";
-
-fetchMovies("avatar").then((data) => {
-  console.log("MOVIES:", data);
-});
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
