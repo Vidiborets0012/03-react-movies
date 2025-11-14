358c4f48ca34647b57bc5c5e57bbb7720c2ee8a8
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-14 19:19:43 +0200

    create and add the SearchBar component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,25 @@
+import fetchMovies from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import SearchBar from "../SearchBar/SearchBar";
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSearch = async (searchQuery: string) => {
+    try {
+      const fetchedMovies = await fetchMovies(searchQuery);
+      setMovies(fetchedMovies);
+      console.log("fetchedMovies:", fetchedMovies);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("finally");
+    }
+  };
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+    </>
+  );
 }
