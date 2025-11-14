7e2f09928343bba983da6b407d1506e945c3a74d
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-14 19:41:31 +0200

    add the Loader and ErrorMessage components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,40 @@
+import toast, { Toaster } from "react-hot-toast";
 import fetchMovies from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 import SearchBar from "../SearchBar/SearchBar";
 import { useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Loader from "../Loader/Loader";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = async (searchQuery: string) => {
     try {
+      setMovies([]);
+      setIsError(false);
+      setIsLoading(true);
       const fetchedMovies = await fetchMovies(searchQuery);
       setMovies(fetchedMovies);
-      console.log("fetchedMovies:", fetchedMovies);
-    } catch (error) {
-      console.log(error);
+
+      if (fetchedMovies.length === 0) {
+        toast.error("No movies found for your request.");
+      }
+    } catch {
+      setIsError(true);
     } finally {
-      console.log("finally");
+      setIsLoading(false);
     }
   };
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+      {isError && <ErrorMessage />}
+      {isLoading && <Loader />}
+
+      <Toaster position="top-center" />
     </>
   );
 }
